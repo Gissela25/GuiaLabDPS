06a73e63d930fdee1ec8630fe0346f55d815a264
Gissela25 <gisselaverenice@gmail.com>
2022-10-10 01:01:58 -0600

    complementario Guia-8

---
+++ guia08/ecomplementario/src/components/Pais.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { Card } from 'react-native-elements';
+const Pais = ({ resultado, busqueda }) => {
+
+  const [info, setinfo] = useState([]);
+  const [nombre, setnombre] = useState();
+  const [capital, setcapital] = useState();
+  const [region, setregion] = useState();
+  const [lengua, setlengua] = useState([]);
+  const [kilometros, setkilometros] = useState();
+  const [metros, setMetros] = useState();
+  const [Unidades, setUnidades] = useState();
+  const { pais } = busqueda
+
+  // const bandera = () =>{
+  //   if(pais == 'ar'){
+      
+  //   }
+  // }
+  useEffect(() => {
+    setinfo(resultado);
+    lengua.length = 0;
+    Object.values(info).map(e => {
+      setnombre(e.nome.abreviado);
+      setcapital(e.governo.capital.nome);
+      setregion(e.localizacao.regiao.nome);
+      const km = (e.area.total);
+      const m = km * 1000000;
+      setkilometros(km);
+      setMetros(m);
+      setUnidades(e.area.unidade.símbolo);
+      Object.values(e.linguas).map(l => {
+        lengua.push(l.nome)
+      })
+
+    }
+    );
+
+  });
+
+  return (
+    <Card>
+      <Card.Title>{nombre}</Card.Title>
+      <Card.Divider />
+      <View style={{ justifyContent: 'center' }}>
+        <Text>Capital:{capital}</Text>
+        <Text>Region:{region}</Text>
+        <Text>Lengua:{lengua.toString()}</Text>
+        <Text>Extension Territorial:{kilometros} {Unidades} </Text>
+        <Text>Extension Territorial:{metros} m2 </Text>
+        <View style={styles.box}>
+          <Image
+            style={styles.Img}
+            source={require('../Img/sv.png')}
+          />
+        </View>
+      </View>
+    </Card>
+
+  );
+};
+
+export default Pais;
+const styles = StyleSheet.create({
+  Img: {
+    marginVertical: 3,
+    width: 200,
+    height: 110
+  },
+  box: {
+    marginTop: 4,
+    alignContent: 'center',
+    alignItems: 'center'
+  },
+})
